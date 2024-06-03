f4260c928c64ebdda67975ffffc7a5b2d46f1650
Melina Lazaro <lazaromelinaceleste@gmail.com>
2024-06-03 14:59:05 +0200

    mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import MiPrimerComponente from "./MiPrimerComponente";
+
+function App() {
+  console.log("HOLA DESDE REACT");
+  return (
+    <>
+      <h1>Hola Mundo desde React</h1>
+      <p>Te queremos</p>
+      <MiPrimerComponente></MiPrimerComponente>
+    </>
+  );
+}
+
+export default App;
---
+++ src/MiPrimerComponente.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function MiPrimerComponente() {
+  console.log("HOLA DESDE REACT");
+  return (
+    <>
+      <h1>Hola, soy un componente</h1>
+      <p>
+        Vos podes <main>Meli</main>
+      </p>
+    </>
+  );
+}
+
+export default MiPrimerComponente;
